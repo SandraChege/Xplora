903a752522cee73a106b7bbb426b2983e4550bea
Sandra Chege <chegemwihaki88@gmail.com>
2023-11-15 05:23:22 +0300

    adds frontend and backend

---
+++ backend/src/Database/procedures/updateUserDetails.sql
@@ -0,0 +1,14 @@
+USE xploraTours
+
+-- SELECT * from Users
+
+-- CREATE OR ALTER PROCEDURE updateUserDetails
+-- @newUserName VARCHAR(200),
+-- @email VARCHAR (200),
+-- @newphone_no VARCHAR(200)
+-- AS
+-- BEGIN
+--     UPDATE Users
+--     SET phone_no = @newphone_no, userName = @newUserName
+--     WHERE email = @email
+-- END
